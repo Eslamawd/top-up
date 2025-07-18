cccfc341fd77c1e91e5a483013d734bfdf2b3933
eslamawd <eeslamawood@gmail.com>
2025-07-18 18:36:21 +0300

    New Intgrition API

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-
 import tailwindcss from '@tailwindcss/vite'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [
     react(),
     tailwindcss(),
   ],
-  
+  server: {
+    port: 5174,  // ✅ استخدم بورت غير مستخدم
+    host: true   // ✅ ضروري لتشغيل التطبيق من خارج localhost (على CloudPanel)
+  }
 })
